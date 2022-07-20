9e30dff971f5e322bdd58564663a5f0097521ee2
Maary <24504742+Steve-Mr@users.noreply.github.com>
2022-07-20 22:11:49 +0800

    1.3.7 获取地图图片png初步实现

--- gs-robot/data/map_png.js
+++ gs-robot/data/map_png.js
@@ -4,9 +4,16 @@ const express = require('express');
 const router = express.Router();
 
 
- router.get('/map_png', (req, res) => {
- var path=require('path');
-  res.sendFile(path.resolve(__dirname,'./map.png'));
- })
+//  router.get('/map_png', (req, res) => {
+//  var path=require('path');
+//   res.sendFile(path.resolve(__dirname,'./map.png'));
+//  })
+
+router.get('/map_png', async function(req, res){
+    let map_name = req.query.map_name;
+
+    let path = require('path');
+    res.sendFile(path.resolve(__dirname, './' + map_name + '.png'));
+});
  
 module.exports=router

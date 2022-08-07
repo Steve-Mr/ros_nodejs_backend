09ff02c0216bf4a93243ab40f278ceeb2e7c89e6
Maary <24504742+Steve-Mr@users.noreply.github.com>
2022-08-07 19:16:24 +0800

    更新初始化节点 api 返回成功/失败状态

--- gs-robot/cmd/init_rosnode.js
+++ gs-robot/cmd/init_rosnode.js
@@ -8,8 +8,15 @@ router.get('/init_rosnode', (req, res) => {
     rosnodejs.initNode('navigation_node')
     rosnodejs.reset()
     rosnodejs.shutdown()
-    rosnodejs.initNode('navigation_node')
-    res.json(util.successed_json)
+    rosnodejs.initNode('navigation_node', {timeout:0})
+    .then(()=>{
+        res.json(util.successed_json)
+    })
+    .catch((err)=>{
+        console.log(err)
+        res.json(util.error_json)
+    })
+    
 })
 
 module.exports = router

e1cf04c3c5d83456959a6771cb4118688062efd3
Maary <24504742+Steve-Mr@users.noreply.github.com>
2022-08-16 22:10:11 +0800

    初步修改实时位置 api

--- gs-robot/real_time_data/position.js
+++ gs-robot/real_time_data/position.js
@@ -5,41 +5,18 @@
 
  const express = require('express');
  const router = express.Router();
- 
- /**
-  * get: 中间件适用的 HTTP 方法
-  * function(req. res, next): 中间件函数，这里代码中使用了 lambda 表达式简写
-  *      req: HTTP 请求自变量
-  *      res: HTTP 响应自变量
-  *      next: 中间件函数的回调自变量
-  * 以上 req, res, next 名称均为通常约定名称。
- */
- router.get('/tf_pos', (req, res) => {
+
+ router.get('/position', (req, res) => {
  
    const rosnodejs = require('rosnodejs');
    const correct = JSON.parse('{"errorCode":"","msg":"successed","successed":true}');
    let message;
- 
-   /**
-    * Promise 对象用于表示一个异步操作的最终完成（或失败）及其结果值
-    * 创建的 Promise 最终将以以解决（resolved）或拒绝状态（rejected）结束
-    * 并在完成时调用相应的回调函数（传递给 then 或 catch）
-   */
+
    let p = new Promise(function (resolve, reject) {
      // 创建名字为 navigation_node 的节点，可能有同一时间只能有一个节点的限制（不确定）
      rosnodejs.initNode('/navigation_node').then(() => {
        const nh = rosnodejs.nh;
-       /**
-        * subscribe(topic, type, callback, options={})
-        * @param topic: 订阅话题名称
-        * @param type: 订阅话题中消息的类型名
-        * @param callback: 获取到消息后的回调函数，下面代码中同样适用了 lambda 表达式简化
-        * @param options: 选项对象，下面代码中未使用
-        * 
-       */
-    //   while(1){
 
-    //   }
        const sub = nh.subscribe('/tf', 'tf/tfMessage', (msg) => {
         // let msg_string = JSON.stringify(msg)
         if(JSON.stringify(msg).includes('"child_frame_id":"base_footprint"')){
@@ -52,11 +29,43 @@
      });
    })
    p.then(function (data) {
+
+    let msg = {
+      "angle":-173.41528128678252,
+      "gridPosition":{
+          "x":372,
+          "y":502
+      },
+      "mapInfo":{
+          "gridHeight":992,
+          "gridWidth":992,
+          "originX":-24.8,
+          "originY":-24.8,
+          "resolution":0.05000000074505806
+      },
+      "worldPosition":{
+          "orientation":{
+              "w":-0.05743089347363588,
+              "x":0,
+              "y":0,
+              "z":0.9983494841361015
+          },
+          "position":{
+              "x":-6.189813393986145,
+              "y":0.3017086724551712,
+              "z":0
+          }
+      }
+  }
+
+  msg.gridPosition.x = data.transforms.translation.x;
+  msg.gridPosition.y = data.transforms.translation.y;
+
      message = {
        "data": JSON.parse(JSON.stringify(data)), "errorCode": "",
        "msg": "successed", "successed": true
      }
-     res.send(JSON.stringify(message))
+     res.send(msg)
    });
  })
  

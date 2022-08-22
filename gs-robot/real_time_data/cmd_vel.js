a40fc6217fff7f83d83562f87a4122f89df771b1
Maary <24504742+Steve-Mr@users.noreply.github.com>
2022-08-22 20:50:36 +0800

    尝试修复 bug

--- gs-robot/real_time_data/cmd_vel.js
+++ gs-robot/real_time_data/cmd_vel.js
@@ -54,11 +54,10 @@ router.get('/cmd_vel', (req, res) => {
   //   }
   //   res.send(JSON.stringify(message))
   // });
-  console.log('进程ID', process.pid);
-  console.log('标题', process.title);
   const child_process = require('child_process')
   const fun = child_process.fork('./gs-robot/real_time_data/cmd_vel_function.js')
   fun.on('message', (msg) =>  {
+    fun.kill();
     res.json(msg)
   })
 })

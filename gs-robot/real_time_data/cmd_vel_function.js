92ec8acbc1b8b3483530fed27447ed2be5c23be6
Maary <24504742+Steve-Mr@users.noreply.github.com>
2023-05-17 21:08:59 +0800

    更新 cmd_vel 主题

--- gs-robot/real_time_data/cmd_vel_function.js
+++ gs-robot/real_time_data/cmd_vel_function.js
@@ -1,19 +1,19 @@
 const rosnodejs = require('rosnodejs');
 let message;
-
+const util = require('../util')
 
 let p = new Promise(function (resolve, reject) {
 
   rosnodejs.initNode('/realtime_subscriber').then(() => {
     const nh = rosnodejs.nh;
 
-    const sub = nh.subscribe('/robot_base_velocity_controller/cmd_vel', 'geometry_msgs/Twist', (msg) => {
+    const sub = nh.subscribe(util.topic_cmd_vel, 'geometry_msgs/Twist', (msg) => {
       resolve(msg)
     });
   });
 })
 p.then(function (data) {
-  rosnodejs.nh.unsubscribe('/robot_base_velocity_controller/cmd_vel');
+  rosnodejs.nh.unsubscribe(util.topic_cmd_vel);
   message = {
     "data": JSON.parse(JSON.stringify(data)), "errorCode": "",
     "msg": "successed", "successed": true

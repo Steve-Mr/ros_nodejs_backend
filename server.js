7477f731162a7f8c1de7c26ac76e101bf807db8b
Maary <24504742+Steve-Mr@users.noreply.github.com>
2023-06-17 22:58:08 +0800

    rclnodejs demo

--- server.js
+++ server.js
@@ -2,20 +2,32 @@
 // 导入 express
 const express = require('express');
 
+console.log("1");
+
 const ws = require('ws')
 const wss = new ws.Server({noServer: true});
 wss.on('connection', socket => {
   socket.on('message', message => console.log(message))
 })
 
+console.log("2");
+
 
 // 创建 express实例，也就是创建 express服务器
 const app = express();
+console.log("4");
+
 
 app.use('/gs-robot', require('./gs-robot/real_time_data'))
+console.log("5");
+
 // 路由器注册，收到请求，启用对应路由
-app.use('/gs-robot', require('./gs-robot/data'))
-app.use('/gs-robot', require('./gs-robot/cmd'))
+// app.use('/gs-robot', require('./gs-robot/data'))
+console.log("6");
+
+// app.use('/gs-robot', require('./gs-robot/cmd'))
+
+console.log("7");
 
 
 const server = app.listen(8089);
@@ -24,6 +36,8 @@ server.on('upgrade', (request, socket, head) => {
     wss.emit('connection', socket, request)
   })
 })
+
+console.log("3");
 
 // 启动服务器
 // 监听 8080 端口
